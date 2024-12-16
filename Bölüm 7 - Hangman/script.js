68f77d3e09f76dda3d2ca4e5e6e39278efbecc61
Barış Karakaya <baybarisk@outlook.com>
2024-12-16 20:41:44 +0300

    -> Timeout kullanımı. -> Tekrar basılan harflerin mesaj ile geri bildirimi.

--- Bölüm 7 - Hangman/script.js
+++ Bölüm 7 - Hangman/script.js
@@ -3,106 +3,119 @@ const popup = document.getElementById("popup-container");
 const message = document.getElementById("message-success");
 const wrongLettersElement = document.getElementById("wrong-letters");
 const items = document.querySelectorAll(".item");
+const messageElement = document.getElementById("message");
 
-function getRandomWord(){
-    const words = ["java","javascript","python"];
+function getRandomWord() {
+  const words = ["java", "javascript", "python"];
 
-    return words[Math.floor(Math.random() * words.length)];
+  return words[Math.floor(Math.random() * words.length)];
 }
 
 const correctLetters = [];
 const wrongLetters = [];
 const selectedWord = getRandomWord();
 
-function displayWord(){
-    wordElement.innerHTML = `
-    ${selectedWord.split("").map(letter =>
-        `
+function displayWord() {
+  wordElement.innerHTML = `
+    ${selectedWord
+      .split("")
+      .map(
+        (letter) =>
+          `
             <div class="letter">
                 ${correctLetters.includes(letter) ? letter : ""}
             </div>
         `
-    ).join("")}`;
+      )
+      .join("")}`;
 
-    /**
-     * /\n/g ifadesi düzenli ifadeler (regular expressions) kullanılarak yazılmış bir modeldir. 
-     * Bu tür ifadeler metin içindeki belirli desenleri tanımlamak, eşleştirmek ve manipüle etmek için kullanılır.
-     * Açıklaması:
-        * /: Düzenli ifadeyi başlatır ve bitirir.
-        * \n: Yeni satır karakterini temsil eder (line feed).
-        * g: Global bayrak; metindeki tüm eşleşmeleri bulur, sadece ilkini değil.
-     */
-    const output = wordElement.innerText.replace(/\n/g,"")
-    if(output === selectedWord){
-        popup.style.display = "flex";
-        message.innerText = "Congratulations! You Made It.";
-    }
+  /**
+   * /\n/g ifadesi düzenli ifadeler (regular expressions) kullanılarak yazılmış bir modeldir.
+   * Bu tür ifadeler metin içindeki belirli desenleri tanımlamak, eşleştirmek ve manipüle etmek için kullanılır.
+   * Açıklaması:
+   * /: Düzenli ifadeyi başlatır ve bitirir.
+   * \n: Yeni satır karakterini temsil eder (line feed).
+   * g: Global bayrak; metindeki tüm eşleşmeleri bulur, sadece ilkini değil.
+   */
+  const output = wordElement.innerText.replace(/\n/g, "");
+  if (output === selectedWord) {
+    popup.style.display = "flex";
+    message.innerText = "Congratulations! You Made It.";
+  }
 }
 
-function updateWrongLetters(){
-    wrongLettersElement.innerHTML = `
-        ${wrongLetters.length > 0? "<h3>Wrong Letters</h3>" : ""}
-        ${wrongLetters.map(letter => `
+function updateWrongLetters() {
+  wrongLettersElement.innerHTML = `
+        ${wrongLetters.length > 0 ? "<h3>Wrong Letters</h3>" : ""}
+        ${wrongLetters.map(
+          (letter) => `
                 <span>${letter}</span>
-            `)}
+            `
+        )}
     `;
 
-    items.forEach((item,index) => {
-        const errorCount = wrongLetters.length;
+  items.forEach((item, index) => {
+    const errorCount = wrongLetters.length;
 
-        item.style.display = (index < errorCount) ? "block" : "none";
-    })
+    item.style.display = index < errorCount ? "block" : "none";
+  });
 
-    if(wrongLetters.length === items.length){
-        popup.style.display = "flex";
-        message.innerText = ":(((( You Failed:"
-    }
-
+  if (wrongLetters.length === items.length) {
+    popup.style.display = "flex";
+    message.innerText = ":(((( You Failed:";
+  }
 }
 
-window.addEventListener("keydown", function(e){
-    if(e.keyCode >= 65 && e.keyCode <= 90){
-        const letter = e.key;
-        // findLetter(selectedWord, letter);
-        if(selectedWord.includes(letter)){
-            if(!correctLetters.includes(letter)){
-                correctLetters.push(letter);
-                displayWord();
-            }
-            else{
-                console.log(`You already entered ${letter} letter`);
-            }
-            console.log(correctLetters);
-        }
-        else {
-            if(!wrongLetters.includes(letter)){
-                wrongLetters.push(letter);
-                updateWrongLetters();
-            }
-            else{
-                updateWrongLetters();
-            }
-            console.log(wrongLetters);
-        }
+function displayMessage(letter) {
+  messageElement.classList.add("show");
+  messageElement.innerHTML = `
+        <p>You Already Entered ${letter} Letter!</p>
+    `;
+
+  setTimeout(function () {
+    messageElement.classList.remove("show");
+  }, 2000);
+}
+
+window.addEventListener("keydown", function (e) {
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+    // findLetter(selectedWord, letter);
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        displayMessage(letter);
+        // console.log(`You already entered ${letter} letter`);
+      }
+    //   console.log(correctLetters);
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+        updateWrongLetters();
+      } else {
+        displayMessage(letter);
+      }
+      console.log(wrongLetters);
     }
-})
+  }
+});
 
 displayWord();
 
-
-
 // My Method....
 function findLetter(word, key) {
-    // 'split' ile stringi bir karakter dizisine dönüştür.
-    word.split('').forEach(function(letter) {
-        if (letter == key) {
-            correctLetters.push(letter);
-            console.log(`Correct Letter: ${letter}`);
-        }
-    });
-
-    let correctWord =  correctLetters.join("");
-    if(correctWord == word){
-        console.log(correctWord);
+  // 'split' ile stringi bir karakter dizisine dönüştür.
+  word.split("").forEach(function (letter) {
+    if (letter == key) {
+      correctLetters.push(letter);
+      console.log(`Correct Letter: ${letter}`);
     }
+  });
+
+  let correctWord = correctLetters.join("");
+  if (correctWord == word) {
+    console.log(correctWord);
+  }
 }
