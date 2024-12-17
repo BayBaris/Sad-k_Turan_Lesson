06f850c3187a47baef646984814101d970691cff
Barış Karakaya <baybarisk@outlook.com>
2024-12-17 15:16:58 +0300

    -> Düzenlendi ancak eski haline geri döndürüldü

--- Bölüm 4 - Form Validator/script.js
+++ Bölüm 4 - Form Validator/script.js
@@ -5,14 +5,18 @@ const password = document.getElementById("password");
 const repassword = document.getElementById("repassword");
 const phone = document.getElementById("phone");
 
-function checkEmail(input){
-    const re =
-  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+var phoneMask = "(___)_______"; // Global olarak tanımla
+var country = "TR"; // Ülke kodu başlangıç değeri
 
-  if(re.test(input.value)){
+updatePattern(country);
+function checkEmail(input) {
+  const re =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  if (re.test(input.value)) {
     success(input);
-  }else{
-    error(input, "Email is not valid!")
+  } else {
+    error(input, "Email is not valid!");
   }
 }
 
@@ -29,52 +33,51 @@ function success(input) {
 }
 
 function checkRequired(inputs) {
-    inputs.forEach(function(input){
-        if(input.value === ""){
-            error(input, `${input.id} alanı boş bırakılamaz!`);
-          }
-          else{
-            success(input);
-          }
-    })
+  inputs.forEach(function (input) {
+    if (input.value === "") {
+      error(input, `${input.id} alanı boş bırakılamaz!`);
+    } else {
+      success(input);
+    }
+  });
 }
 
-function checkLength(input, minL, maxL){
-    if(input.value.length < minL){
-        error(input, `Minimum character value is ${minL}!`);
-    }
-    else if(input.value.length > maxL){
-        error(input, `Your character limit (${input.value.length}) is over ${maxL}!`)
-    }
-    else{
-        success(input);
-    }
+function checkLength(input, minL, maxL) {
+  if (input.value.length < minL) {
+    error(input, `Minimum character value is ${minL}!`);
+  } else if (input.value.length > maxL) {
+    error(
+      input,
+      `Your character limit (${input.value.length}) is over ${maxL}!`
+    );
+  } else {
+    success(input);
+  }
 }
 
-function checkPasswords(pass1,pass2){
-    if(pass1.value !== pass2.value){
-        error(pass2, "Passwords doesn't match!");
-    }
-    else{
-        success(pass2);
-    }
+function checkPasswords(pass1, pass2) {
+  if (pass1.value !== pass2.value) {
+    error(pass2, "Passwords doesn't match!");
+  } else {
+    success(pass2);
+  }
 }
 
-function checkPhone(input){
-    var exp = /^\d{10}$/;
-    if(!exp.test(input.value)){
-        error(input,"Phone number is must be 10 character!");
-    }
-    else{
-        success(input);
-    }
+function checkPhone(input) {
+  var exp = /^\+90\(\d{3}\)\d{7}$/;
+  if (!exp.test(input.value)) {
+    error(input, "Phone number format must be +90(552)2653192 !");
+  } else {
+    success(input);
+  }
 }
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  checkRequired([username,email,password,repassword]);
+  checkRequired([username, email, password, repassword]);
   checkEmail(email);
   checkLength(username, 6, 16);
   checkPasswords(password, repassword);
   checkPhone(phone);
 });
+
