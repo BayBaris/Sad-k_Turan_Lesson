13672a363af80edb73bd7c0b2b8afa3349b9d475
Barış Karakaya <baybarisk@outlook.com>
2024-12-18 15:16:02 +0300

    -> Seçeneklerin seçilmesi, renklendirilmesi ve gerekli kontroller -> İleri Tuşunun Eklenmesi -> Hangi soruda olduğu bilgisinin gösterilmesi

--- Bölüm 8 - Quiz App/script.js
+++ Bölüm 8 - Quiz App/script.js
@@ -1,68 +1,127 @@
 function Question(questionText, answerOptions, correctAnswer) {
-    this.questionText = questionText;
-    this.answerOptions = answerOptions;
-    this.correctAnswer = correctAnswer;
-    this.checkAnswer = function (answer) {
-        return answer === this.correctAnswer
-    }
-    // console.log(this)
+  this.questionText = questionText;
+  this.answerOptions = answerOptions;
+  this.correctAnswer = correctAnswer;
+  this.checkAnswer = function (answer) {
+    return answer === this.correctAnswer;
+  };
+  // console.log(this)
 }
 
 Question.prototype.checkAnswer2 = function (answer) {
-    return answer === this.correctAnswer
-}
+  return answer === this.correctAnswer;
+};
 
 let questions = [
-    new Question(
-        "1) Which one is a .net package management application?",
-        {
-            a: "Node.js",
-            b: "Nuget",
-            c: "Npm",
-        },
-        "b"
-    ),
-    new Question(
-        "2) Which one is a .net package management application?",
-        {
-            a: "Node.js",
-            b: "Nuget",
-            c: "Npm",
-        },
-        "a"
-    ),
-    new Question(
-        "3) Which one is a .net package management application?",
-        {
-            a: "Node.js",
-            b: "Nuget",
-            c: "Npm",
-        },
-        "c"
-    ),
+  new Question(
+    "1) Which one is a .net package management application?",
+    {
+      a: "Node.js",
+      b: "Nuget",
+      c: "Npm",
+      d: "Nuget",
+    },
+    "b"
+  ),
+  new Question(
+    "2) Which one is a .net package management application?",
+    {
+      a: "Node.js",
+      b: "Nuget",
+      c: "Npm",
+    },
+    "a"
+  ),
+  new Question(
+    "3) Which one is a .net package management application?",
+    {
+      a: "Node.js",
+      b: "Nuget",
+      c: "Npm",
+    },
+    "c"
+  ),
 ];
 
-function Quiz(questions){
-    this.questions = questions;
-    this.questionIndex = 0;
+function Quiz(questions) {
+  this.questions = questions;
+  this.questionIndex = 0;
 }
 
-
-Quiz.prototype.getQuestion = function(){
-    return this.questions[this.questionIndex];
-}
+Quiz.prototype.getQuestion = function () {
+  return this.questions[this.questionIndex];
+};
 
 const quiz = new Quiz(questions);
+
 const quizBox = document.querySelector(".quiz-box");
+const optionList = document.querySelector(".option-list");
+const nextBtn = document.querySelector(".next-btn");
 
-document.querySelector(".btn-start").addEventListener("click",function(){
-    if(quiz.questions.length != quiz.questionIndex){
-        console.log(quiz.getQuestion());
-        quizBox.classList.add("active");
+const correctIcon = '<div class="icon"><i class="fas fa-check"></i></div>';
+const wrongIcon = '<div class="icon"><i class="fas fa-times"></i></div>';
 
-        quiz.questionIndex += 1;
-    }
-    else{
-        console.log("Quiz Bitti")
-    }
+document.querySelector(".btn-start").addEventListener("click", function () {
+  quizBox.classList.add("active");
+  showQuestion(quiz.getQuestion());
+  showQuestionNumber(quiz.questionIndex + 1, quiz.questions.length);
+  nextBtn.classList.remove("show");
 });
+
+document.querySelector(".next-btn").addEventListener("click", function () {
+  if (quiz.questions.length != quiz.questionIndex + 1) {
+    quiz.questionIndex += 1;
+    showQuestion(quiz.getQuestion());
+    showQuestionNumber(quiz.questionIndex + 1, quiz.questions.length);
+
+    nextBtn.classList.remove("show");
+  } else {
+    console.log("Quiz Bitti");
+  }
+});
+
+function showQuestion(que) {
+  let question = `<span>${que.questionText}</span>`;
+  let option = ``;
+
+  for (let answer in que.answerOptions) {
+    option += `
+            <div class="option"> 
+                <span><b>${answer}</b>: ${que.answerOptions[answer]}</span>
+                </div>
+        `;
+  }
+
+  document.querySelector(".question-text").innerHTML = question;
+  optionList.innerHTML = option;
+
+  const options = optionList.querySelectorAll(".option");
+
+  for (let option of options) {
+    option.setAttribute("onclick", "optionSelected(this)");
+  }
+}
+
+function optionSelected(option) {
+  let answer = option.querySelector("span b").textContent;
+  let que = quiz.getQuestion();
+
+  if (que.checkAnswer2(answer)) {
+    option.classList.add("correct");
+    option.insertAdjacentHTML("beforeend", correctIcon);
+  } else {
+    option.classList.add("incorrect");
+    option.insertAdjacentHTML("beforeend", wrongIcon);
+  }
+
+  for (let i = 0; i < optionList.children.length; i++) {
+    optionList.children[i].classList.add("disabled");
+  }
+
+  nextBtn.classList.add("show");
+}
+
+function showQuestionNumber(queQueue, totalQue) {
+  let tag = `<span class="badge bg-warning">${queQueue} / ${totalQue}</span>`;
+  document.querySelector(".quiz-box .question-index").innerHTML = tag;
+}
