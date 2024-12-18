db6f6af7fac49fa5241c655d04d646a5e2fc308a
Barış Karakaya <baybarisk@outlook.com>
2024-12-18 12:00:01 +0300

    -> Prototype ve Class denemeleri

--- Bölüm 8 - Quiz App/script.js
+++ Bölüm 8 - Quiz App/script.js
@@ -1,30 +1,71 @@
-// OOP
+function Question(questionText, answerOptions, correctAnswer) {
+    this.questionText = questionText;
+    this.answerOptions = answerOptions;
+    this.correctAnswer = correctAnswer;
+    this.checkAnswer = function (answer) {
+        return answer === this.correctAnswer
+    }
 
-let question1 = {
-    text: "Which one is a javascript package management application?",
-    options : {
+    console.log(this)
+}
+
+Question.prototype.checkAnswer2 = function (answer) {
+    return answer === this.correctAnswer
+}
+
+let que1 = new Question(
+    "Which one is a javascript package management application?",
+    {
         a: "Node.js",
         b: "Typescript",
-        c: "Npm"
+        c: "Npm",
     },
-    correctAnswer: "c",
-    checkAnswer: function(answer){
-        return answer === this.correctAnswer;
-    }
-};
+    "c"
+);
 
-let question2 = {
-    text: "Which one is a .net package management application?",
-    options : {
+let que2 = new Question(
+    "Which one is a .net package management application?",
+    {
         a: "Node.js",
         b: "Nuget",
-        c: "Npm"
+        c: "Npm",
     },
-    correctAnswer: "b",
-    checkAnswer: function(answer){
-        return answer === this.correctAnswer;
-    }
-};
+    "b"
+);
 
-console.log(question1.checkAnswer("a"));
-console.log(question2.options);
+let questions = [
+    new Question(
+        "Which one is a .net package management application?",
+        {
+            a: "Node.js",
+            b: "Nuget",
+            c: "Npm",
+        },
+        "b"
+    ),
+    new Question(
+        "Which one is a .net package management application?",
+        {
+            a: "Node.js",
+            b: "Nuget",
+            c: "Npm",
+        },
+        "a"
+    ),
+    new Question(
+        "Which one is a .net package management application?",
+        {
+            a: "Node.js",
+            b: "Nuget",
+            c: "Npm",
+        },
+        "c"
+    ),
+];
+// console.log(que1.correctAnswer);
+
+// for (let que of questions) {
+//     console.log(que.correctAnswer);
+// }
+
+// console.log(questions[0].checkAnswer("c"));
