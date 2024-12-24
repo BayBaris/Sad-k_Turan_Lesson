b5cdd0ae83d6de8b1affd20e6eae846bfccbfb8f
Barış Karakaya <baybarisk@outlook.com>
2024-12-24 17:20:20 +0300

    -> Progress Bar +

--- Bölüm 11 - Music Player/app.js
+++ Bölüm 11 - Music Player/app.js
@@ -6,6 +6,9 @@ const audio = document.getElementById("music");
 const prev = document.getElementById("prev");
 const play = document.getElementById("play");
 const next = document.getElementById("next");
+const duration = document.getElementById("duration");
+const currentTime = document.getElementById("current-time");
+const progressBar = document.getElementById("progress-bar");
 
 
 
@@ -50,6 +53,16 @@ function nextMusic(){
     playMusic();
 }
 
+const calculateTime = (duration) => {
+    const minute = Math.floor(duration / 60);
+    const second = Math.floor(duration % 60);
+    const updatedSecond = second < 10 ? `0${second}` : `${second}`
+    const time = `
+        ${minute}:${updatedSecond} 
+    `;
+    return time;
+}
+
 play.addEventListener("click", () => {
     const isMusicPlay = container.classList.contains("playing")
     isMusicPlay ? pauseMusic() : playMusic()
@@ -61,4 +74,15 @@ prev.addEventListener("click", () => {
 
 next.addEventListener("click", () =>{
     nextMusic();
+})
+
+audio.addEventListener("loadedmetadata",()=>{
+    console.log(audio.duration);
+    duration.textContent = calculateTime(audio.duration);
+    progressBar.max = Math.floor(audio.duration);
+})
+
+audio.addEventListener("timeupdate", ()=>{
+    progressBar.value = Math.floor(audio.currentTime);
+    currentTime.textContent = calculateTime(progressBar.value);
 })
