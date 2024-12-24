81e36963ab12e6475d741ce69ab5ffca51753123
Barış Karakaya <baybarisk@outlook.com>
2024-12-24 16:14:21 +0300

    -> Ekranda Gösterim ve Play Tuşu +

--- Bölüm 11 - Music Player/app.js
+++ Bölüm 11 - Music Player/app.js
@@ -1,11 +1,35 @@
+const container = document.querySelector(".container");
+const image = document.getElementById("music-image");
+const title = document.querySelector(".title");
+const singer = document.querySelector(".singer");
+const audio = document.getElementById("music");
+const prev = document.getElementById("prev");
+const play = document.getElementById("play");
+const next = document.getElementById("next");
+
+
+
 const player = new MusicPlayer(musicList);
 
-let music = player.getMusic();
+window.addEventListener("load", ()=>{
+    let music = player.getMusic();
+    displayMusic(music);
+})
 
-console.log(music.getName())
-player.next();
-music = player.getMusic();
-console.log(music.getName())
-player.previous();
-music = player.getMusic();
-console.log(music.getName())
+
+function displayMusic(music){
+    title.innerText = music.getName();
+    singer.innerText = music.singer;
+    image.src = "img/" + music.img;
+    audio.src = "mp3/" + music.file;
+}
+
+play.addEventListener("click", () => {
+    audio.play();
+})
+
+next.addEventListener("click", () =>{
+    player.next();
+    let music = player.getMusic();
+    displayMusic(music);
+})
