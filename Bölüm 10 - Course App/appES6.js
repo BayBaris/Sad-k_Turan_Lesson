9109ac08e9dc25874fd614b5a93cb68932de6657
Barış Karakaya <baybarisk@outlook.com>
2024-12-24 14:22:19 +0300

    -> Local Storage Deleting -

--- Bölüm 10 - Course App/appES6.js
+++ Bölüm 10 - Course App/appES6.js
@@ -1,5 +1,6 @@
 class Course {
   constructor(title, instructor, image) {
+    this.courseID = Math.floor(Math.random() * 10000);
     this.title = title;
     this.instructor = instructor;
     this.image = image;
@@ -15,7 +16,7 @@ class UI {
                 <td><img src="img/${course.image}"/></td>
                 <td>${course.title}</td>
                 <td>${course.instructor}</td>
-                <td><a href="#" class="btn btn-danger btn-sm delete">Delete</td>
+                <td><a href="#" data-id="${course.courseID}" class="btn btn-danger btn-sm delete">Delete</td>
             </tr>
         `;
 
@@ -52,77 +53,81 @@ class UI {
   }
 }
 
-class Storage{
+class Storage {
+  static getCourses() {
+    let courses;
 
-    static getCourses(){
-        let courses;
+    if (localStorage.getItem("courses") === null) {
+      courses = [];
+    } else {
+      courses = JSON.parse(localStorage.getItem("courses"));
+    }
 
-        if(localStorage.getItem('courses') === null){
-            courses = []
+    return courses;
+  }
+
+  static displayCourses() {
+    const courses = Storage.getCourses();
+
+    courses.forEach((course) => {
+      const ui = new UI();
+      ui.addCourseToList(course);
+    });
+  }
+
+  static addCourse(course) {
+    const courses = Storage.getCourses();
+    courses.push(course);
+    localStorage.setItem("courses", JSON.stringify(courses));
+  }
+
+  static deleteCourse(element) {
+    if (element.classList.contains("delete")) {
+      const id = element.getAttribute("data-id");
+      const courses = Storage.getCourses();
+
+      courses.forEach((course, index) => {
+        if (course.courseID == id) {
+          courses.splice(index, 1);
         }
-        else{
-            courses = JSON.parse(localStorage.getItem('courses'))
-        }
+      });
 
-        return courses
+      localStorage.setItem("courses", JSON.stringify(courses));
     }
-
-    static displayCourses(){
-        const courses = Storage.getCourses();
-
-        courses.forEach(course => {
-            const ui = new UI();
-            ui.addCourseToList(course);
-        });
-    }
-
-    static addCourse(course){
-        const courses = Storage.getCourses();
-        courses.push(course);
-        localStorage.setItem('courses', JSON.stringify(courses))
-    }
-
-    static deleteCourse(){
-
-    }
+  }
 }
 
 document.addEventListener("DOMContentLoaded", Storage.displayCourses);
 
-
 const ui = new UI();
-document.getElementById("new-course").addEventListener("submit", function(e){
-    
-    const title = document.getElementById("title").value;
-    const instructor = document.getElementById("instructor").value;
-    const image = document.getElementById("image").value;
+document.getElementById("new-course").addEventListener("submit", function (e) {
+  const title = document.getElementById("title").value;
+  const instructor = document.getElementById("instructor").value;
+  const image = document.getElementById("image").value;
 
-    // Create course object
-    const course = new Course(title,instructor,image);
-    
-    if(title === "" || instructor === "" || image === ""){
-        ui.showAlert("Please complete form!", "warning");
-    }
-    else{
-        // Add to course list
-        ui.addCourseToList(course);
-        
-        // Save to LocalStorage
-        Storage.addCourse(course);
+  // Create course object
+  const course = new Course(title, instructor, image);
 
-        // Clear controls
-        ui.clearControls();
+  if (title === "" || instructor === "" || image === "") {
+    ui.showAlert("Please complete form!", "warning");
+  } else {
+    // Add to course list
+    ui.addCourseToList(course);
 
-        ui.showAlert("Course has been added!", "success")
-    }
-    
-    e.preventDefault();
+    // Save to LocalStorage
+    Storage.addCourse(course);
+
+    // Clear controls
+    ui.clearControls();
+
+    ui.showAlert("Course has been added!", "success");
+  }
+
+  e.preventDefault();
 });
 
-document.getElementById("course-list").addEventListener("click", function(e){
-    ui.deleteCourse(e.target)
-    Storage.deleteCourse();
-    ui.showAlert("You deleted one course", "danger")
-    console.log(e.target);
-
-})
+document.getElementById("course-list").addEventListener("click", function (e) {
+  ui.deleteCourse(e.target);
+  Storage.deleteCourse(e.target);
+  ui.showAlert("You deleted one course", "danger");
+});
