03f6abad7a0ace26910abd82dfb6881dd487a6ea
Barış Karakaya <baybarisk@outlook.com>
2025-01-06 18:44:00 +0300

    -> Controller Tanımlamaları

---
+++ Bölüm 19 - OEM Calculator/app.js
@@ -0,0 +1,28 @@
+// Storage Controller
+const StorageController = (function(){
+
+})();
+
+// Product Controller
+const ProductController = (function(){
+    // private
+
+    return{
+        //public
+    }
+    
+})();
+
+// UI Controller
+const UIController = (function(){
+    
+})();
+
+
+// App Controller
+const AppController = (function(ProcudCtrl, UICtrl){
+
+
+
+    
+})(ProductController, UIController);
