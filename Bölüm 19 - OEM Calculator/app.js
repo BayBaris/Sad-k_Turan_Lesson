be9542de8f0148dff99ef4ae615f4c24c2a94ded
Barış Karakaya <baybarisk@outlook.com>
2025-01-06 18:52:19 +0300

    -> Veri Yapılarının Oluşturulması

--- Bölüm 19 - OEM Calculator/app.js
+++ Bölüm 19 - OEM Calculator/app.js
@@ -6,15 +6,36 @@ const StorageController = (function(){
 // Product Controller
 const ProductController = (function(){
     // private
+    const Product = function(id,name,price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
 
+    const data = {
+        products : [
+            {id: 1, name: "Monitor", price: 1000},
+            {id: 2, name: "Keyboard", price: 400},
+            {id: 3, name: "Mouse", price: 300}
+        ],
+        selectedProduct : null,
+        totalPrice : 0
+    }
     return{
         //public
+        getProducts : function(){
+            return data.products;
+        },
+        
+        getData : function(){
+            return data;
+        }
     }
     
 })();
 
 // UI Controller
-const UIController = (function(){
+const UIController = (function(products){
     
 })();
 
@@ -22,7 +43,17 @@ const UIController = (function(){
 // App Controller
 const AppController = (function(ProcudCtrl, UICtrl){
 
+    return {
+        init: function(){
+            console.log("App Starting...");
+            const products = ProcudCtrl.getProducts();
+            console.log(products);
 
-
+            UICtrl.createProductList(products);
+        }
+    }
     
 })(ProductController, UIController);
+
+
+AppController.init();
