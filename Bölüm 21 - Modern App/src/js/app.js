fdb8d902d9c279d16238df7a57dac3465cf08380
Barış Karakaya <baybarisk@outlook.com>
2025-01-09 14:56:23 +0300

    -> Exporting with Class

--- Bölüm 21 - Modern App/src/js/app.js
+++ Bölüm 21 - Modern App/src/js/app.js
@@ -1,6 +1,6 @@
-// import { sum, multiple} from "./lib";
+import mathLib from "./lib"  
 
-import * as lib from "./lib"
+const lib = new mathLib();
 
 console.log(lib.sum(1,2,3,4,5,6));
 console.log(lib.multiple(2,4,6,7));
--- Bölüm 21 - Modern App/src/js/lib.js
+++ Bölüm 21 - Modern App/src/js/lib.js
@@ -1,16 +1,13 @@
-//private members ...
-const PI = 3.1428;
+export default class {
+    constructor(){
+        this.PI = 3.1428
+    }
 
-//public members...
-export function sum(...numbers) {
-    return numbers.reduce((number, total) => total + number);
-};
+    sum(...numbers) {
+        return numbers.reduce((number, total) => total + number);
+    }
 
-
-export function multiple(...numbers){
-    return numbers.reduce((number, total) => total * number);
+    multiple(...numbers) {
+        return numbers.reduce((number, total) => total * number);
+    }
 }
-
-//ES6
-
-sum(3,2,34)
